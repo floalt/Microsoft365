a933ea20111354c4da137f1df8a7598a2d085052
floalt <flo.alt@fa-netz.de>
2022-02-26 15:35:00 +0100

    the new way

--- login365.ps1
+++ login365.ps1
@@ -1,6 +1,20 @@
 ﻿# Verbindung mit Office365 herstellen
 
+
+<# Das ist alt:
+
 $LiveCred = Get-Credential    # hier Anmeldedaten vom Office365-Admin in PopUp-Fenster eingeben
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
 Import-PSSession $Session -AllowClobber
 Connect-MsolService –Credential $LiveCred
+
+#>
+
+<# Muss man das jedesmal oder nur einmal machen?
+
+    Install-Module -Name ExchangeOnlineManagement
+    Import-Module -Name ExchangeOnlineManagement
+
+#>
+
+Connect-ExchangeOnline
