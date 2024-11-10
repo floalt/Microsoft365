286495663d1f9586ae9c0d3309b03454a18f6a9f
floalt <flo.alt@fa-netz.de>
2024-11-10 14:20:27 +0100

    edit make-perms-2groups

--- make-perms-2groups.ps1
+++ make-perms-2groups.ps1
@@ -2,13 +2,35 @@
 # author: flo.alt@fa-netz.de
 # Ver 0.8
  
-# Verwendung: .\make-perms -mailbox [MailboxName] -suser [Schreib-Gruppe] -luser [Lese-Gruppe]
-# Beispiel: .\make-perms -mailbox einkauf -suser sg-einkauf-s -luser sg-einkauf-l
+# Verwendung: .\make-perms -mailbox [MailboxName] -sgroup [Schreib-Gruppe] -lgroup [Lese-Gruppe]
+# Beispiel: .\make-perms -mailbox einkauf -sgroup sg-einkauf-s -lgroup sg-einkauf-l
 
-param([string]$mailbox,[string]$suser,[string]$luser)# Sprache der Ordnernamen:Set-MailboxRegionalConfiguration $mailbox -Language de-DE -LocalizeDefaultFolderName
-# Hier werden die Schreibrechte festgelegt
-Add-MailboxFolderPermission -Identity $mailbox -User $suser -AccessRights PublishingEditor -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Posteingang" -User $suser -AccessRights PublishingEditor -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Gesendete Elemente" -User $suser -AccessRights PublishingEditor -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Gelöschte Elemente" -User $suser -AccessRights PublishingEditor -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Junk-E-Mail" -User $suser -AccessRights PublishingEditor -Confirm:$False
-# Hier werden die Leserechte festgelegt
-Add-MailboxFolderPermission -Identity $mailbox -User $luser -AccessRights Reviewer -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Posteingang" -User $luser -AccessRights Reviewer -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Gesendete Elemente" -User $luser -AccessRights Reviewer -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Gelöschte Elemente" -User $luser -AccessRights Reviewer -Confirm:$FalseAdd-MailboxFolderPermission -Identity $mailbox":\Junk-E-Mail" -User $luser -AccessRights Reviewer -Confirm:$False
-# Hier Senderechte für suser vergeben
-Add-RecipientPermission -Identity $mailbox -AccessRights SendAs -Trustee $suser -Confirm:$False
+param(
+[string]$mailbox,
+[string]$sgroup,
+[string]$lgroup
+)
+
+# Sprache der Ordnernamen:
+
+Set-MailboxRegionalConfiguration $mailbox -Language de-DE -LocalizeDefaultFolderName
+
+# Hier werden die Schreibrechte festgelegt
+
+Add-MailboxFolderPermission -Identity $mailbox -User $sgroup -AccessRights PublishingEditor -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Posteingang" -User $sgroup -AccessRights PublishingEditor -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Gesendete Elemente" -User $sgroup -AccessRights PublishingEditor -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Gelöschte Elemente" -User $sgroup -AccessRights PublishingEditor -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Junk-E-Mail" -User $sgroup -AccessRights PublishingEditor -Confirm:$False
+
+# Hier werden die Leserechte festgelegt
+
+Add-MailboxFolderPermission -Identity $mailbox -User $lgroup -AccessRights Reviewer -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Posteingang" -User $lgroup -AccessRights Reviewer -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Gesendete Elemente" -User $lgroup -AccessRights Reviewer -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Gelöschte Elemente" -User $lgroup -AccessRights Reviewer -Confirm:$False
+Add-MailboxFolderPermission -Identity $mailbox":\Junk-E-Mail" -User $lgroup -AccessRights Reviewer -Confirm:$False
+
+# Hier Senderechte für sgroup vergeben
+
+Add-RecipientPermission -Identity $mailbox -AccessRights SendAs -Trustee $sgroup -Confirm:$False
